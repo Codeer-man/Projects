982087ecdb477402f8568edc7ec17ad59a42349f
Codeer-man <mdrmoney34@gmail.com>
2025-02-28 23:58:21 +0545

    Blog post validatoin

--- personal-blog/backend/routes/BlodPost-route.js
+++ personal-blog/backend/routes/BlodPost-route.js
@@ -6,12 +6,14 @@ const {
   UpdatePost,
   DeletePost,
 } = require("../controllers/BlogPost-controller");
+const blogValidation = require("../validation/blog-validation");
+const validation = require("../middleware/validation-middleware");
 
 const route = express.Router();
 
 route.get("/getpost", getPost);
 route.get("/getPostbyId/:id", getPostById);
-route.post("/createPost", CreatePost);
+route.post("/createPost", validation(blogValidation), CreatePost);
 route.put("/:id/update", UpdatePost);
 route.delete("/:id/delete", DeletePost);
 
